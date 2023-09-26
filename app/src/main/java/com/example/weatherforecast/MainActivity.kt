10c6a7e3bdea70359a80d563acaccc9fb60839ce
Mohamed Mabrouk <mohamedmabrouk180@gmail.com>
2023-09-27 02:54:54 +0300

    Home Screen: Design Layout

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -3,17 +3,38 @@ package com.example.weatherforecast
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weatherforecast.ui.theme.DarkBlue
 import com.example.weatherforecast.ui.theme.LightBlue
@@ -34,25 +55,63 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(
-                        LightBlue,
-                        DarkBlue
-                    )
-                )
-            )
+    Surface(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Hello!",
-            fontSize = 16.sp,
-            fontFamily = FontFamily.Monospace,
-            color = Color.White
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            LightBlue,
+                            DarkBlue
+                        )
+                    )
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(text = "San Diego", style = MaterialTheme.typography.titleMedium)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.location_pin),
+                    contentDescription = null
+                )
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "Current Location",
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+            Spacer(modifier = Modifier.height(22.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.size(130.dp),
+                    painter = painterResource(id = R.drawable.ic_partly_cloudy),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(27.dp))
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = "13°",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+            Text(
+                modifier = Modifier.padding(0.dp),
+                text = "Partly Cloud - H:17o L:4o",
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
     }
 
 }
--- app/src/main/java/com/example/weatherforecast/ui/theme/Type.kt
+++ app/src/main/java/com/example/weatherforecast/ui/theme/Type.kt
@@ -1,34 +1,46 @@
 package com.example.weatherforecast.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.example.weatherforecast.R
 
-// Set of Material typography styles to start with
+val mainFont = FontFamily(
+    Font(R.font.circular_std)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 120.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight(450),
+        color = Color.White,
+        textAlign = TextAlign.Center
+    ),
+    titleMedium = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight(450),
+        color = Color.White,
+        textAlign = TextAlign.Center
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight(450),
+        color = Color.White,
+        textAlign = TextAlign.Center
+    ),
+    labelMedium = TextStyle(
+        fontSize = 18.sp,
+        fontFamily = mainFont,
+        fontWeight = FontWeight(450),
+        color = Color.White,
+        textAlign = TextAlign.Center
     )
-    */
 )
